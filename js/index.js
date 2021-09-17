7cabeeaf1894a90501e3dddef52b4fd6cb84cc1a
1Enemy <rikisamer@yandex.ru>
2021-09-17 15:13:01 +0300

    Revert "add visible obj"
    This reverts commit 9a04741877e9e2355b8c37e133872881418739b1.

--- js/index.js
+++ js/index.js
@@ -1,40 +1,15 @@
 let baseData = [];
-let obj1 = {}
-
-document.querySelector('#btnSingIn').addEventListener('click', function(){
+document.querySelector('.btn').addEventListener('click', function(){
+    let inp1 = document.querySelector('#inp1').value
+    let inp2 = document.querySelector('#inp2').value
     
-    obj1.username = document.querySelector('#inp1').value
-    obj1.password = document.querySelector('#inp2').value
-    baseData.push(obj1)
+    let obj = {
+        username: inp1,
+        password: inp2,
+    }
+
+    baseData.push(obj)
     console.log(baseData)
+
 })
 
-
-
-document.querySelector('#btnGo').addEventListener('click', function(){
-    baseData.forEach(item=>{
-        for(let key in item){
-                document.getElementById('obj').innerHTML += `
-                <div style='padding-left:20px;'>${key}: ${item[key]}</div>
-            `
-        }
-    })    
-})
-
-
-
-document.querySelector('#newBtnObj').addEventListener('click', function(){
-    let objName = document.querySelector('#objName').value
-    let objVal = document.querySelector('#objVal').value
-    let obj2 = {}
-
-    obj2[objName] = objVal
-    
-    const keys = Object.keys(obj2);
-    keys.forEach(key => {
-        document.getElementById('obj').innerHTML += `<div style='padding-left:20px;'>${key}: ${obj2[key]}</div>`
-        console.log(obj1);
-        console.log(obj2);
-    });
-    Object.assign(obj1, obj2)
-})
